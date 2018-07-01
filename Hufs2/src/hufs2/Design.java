723af482203ed54d24f9158cacb189f4468a1cce
LouSteinberg <lou@cs.rutgers.edu>
2018-07-01 00:49:58 -0400

    First commit

---
+++ Hufs2/src/hufs2/Design.java
@@ -0,0 +1,14 @@
+package hufs2;
+
+public class Design {
+
+	public Design() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+	System.out.println("foo");
+
+	}
+
+}
