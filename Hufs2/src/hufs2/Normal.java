7c4b36556738e5fc266a8001a34cec09448857d2
LouSteinberg <lou@cs.rutgers.edu>
2018-07-02 18:17:09 -0400

    The main in Normal revised to test whether sum of two normals is normal

--- Hufs2/src/hufs2/Normal.java
+++ Hufs2/src/hufs2/Normal.java
@@ -17,20 +17,39 @@ public class Normal extends Distribution {
 		return mean + fRandom.nextGaussian() * stDev;
 	}
 	public static void main(String [ ] args) {
-		Normal dist= new Normal(10.0, 5.0);
-		int ct = 1000;
+		Normal dist = new Normal(5.0, 1.0);
+		Normal diste = new Normal(0.0, 2.0);
+		double ct = 10000;
+		int sig1=0;
+		int sig2=0;
+		int sig3=0;
 		double sum = 0;
 		double sumsq = 0;
 		for (int j = 0; j<ct; j++) {
 			double drawn = dist.draw( );
+			double drawne = diste.draw( );
+			drawn += drawne;
 			sum += drawn;
 			sumsq += drawn*drawn;
-			System.out.format("%g\n",drawn);
+			// System.out.format("%g\n",drawn);
+			double err = drawn-4.96;
+			if (Math.abs(err)< 1.0*2.237) {
+				sig1=sig1+1;
+			}
+			if (Math.abs(err)< 2.0*2.237) {
+				sig2=sig2+1;
+			}
+			if (Math.abs(err)< 3.0*2.237) {
+				sig3=sig3+1;
+			}
 		}
 		double mean = sum/ct;
 		double stDev = Math.sqrt((ct*sumsq-sum*sum)/((ct-1)*ct));
 		System.out.println("ct: "+ct);
 		System.out.println("mean: "+mean);
 		System.out.println("stDev: "+stDev);
+		System.out.println("sig1: "+sig1/ct);
+		System.out.println("sig2: "+sig2/ct);
+		System.out.println("sig3: "+sig3/ct);
 	}
 }
