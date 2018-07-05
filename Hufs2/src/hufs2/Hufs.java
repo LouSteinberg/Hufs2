6446550ac7fc955a15839c179cbda4698e36eea1
LouSteinberg <lou@cs.rutgers.edu>
2018-07-05 16:13:36 -0400

    Work on Hufs main

--- Hufs2/src/hufs2/Hufs.java
+++ Hufs2/src/hufs2/Hufs.java
@@ -3,20 +3,22 @@ import java.util.ArrayList;
 
 
 public class Hufs {
-	public static  void main(String [] args) {
+	public static  void main(String [ ] args) {
 		ArrayList<Design> allDesigns = new ArrayList<Design>( );
-		// Level [ ] levels;
+		int numLevels = 3;		// number of levels
+		Level [ ] levels = Level.initializedLevels(numLevels);
+		int numTopLevel = numLevels - 1;  // index/level number of top level
 		// calculate utility of parent
-		Design specs = new Design(null);
+		Design specs = new Design(levels[numTopLevel]);
 		Design parent = specs;
 		allDesigns.set(0, parent);
-		while (parent.level.levelDown != null) {
-			Design child = parent.generate();
+		while (! parent.level.isBottomLevel()) {
+			Design child = new Design(parent);
 			allDesigns.add(child);
 			parent = findBest(allDesigns);
 		}
 	}
 	public static Design findBest(ArrayList<Design> designs) {
-		return new Design(null);
+		return designs.get(0);
 	}
 }
